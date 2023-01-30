23d370214db77f4ad0ad9876af6d91c9fb883621
o7Moon <0167moon@gmail.com>
2023-01-30 17:55:03 -0600

    Initial commit

---
+++ Plugin.cs
@@ -0,0 +1,74 @@
+﻿using BepInEx;
+using HarmonyLib;
+using UnityEngine;
+
+namespace Flipped;
+
+[BepInPlugin("Meadow.Flipped", "Flipped", "1.0")]
+public class Plugin : BaseUnityPlugin
+{
+    public static Plugin Instance;
+    public static float height = 1;
+    public static bool flipped = true;
+    public static Animal localAnimal;
+    private void Awake()
+    {
+        // Plugin startup logic
+        Instance = this;
+        Harmony.CreateAndPatchAll(typeof(Plugin));
+        Logger.LogInfo($"Plugin {MyPluginInfo.PLUGIN_GUID} is loaded!");
+    }
+    // before the game tells the server our position and rotation, let's modify it to flip the player
+    [HarmonyPatch(typeof(Fuse),nameof(Fuse.Move))]
+    [HarmonyPrefix]
+    public static void MovePacketHook(Fuse __instance, ref string data){
+        if (!flipped) return;
+        string[] splitData = data.Split(',');
+        splitData[1] = (float.Parse(splitData[1])+height).ToString();
+        Quaternion rotation = new Quaternion(float.Parse(splitData[3]),float.Parse(splitData[4]),float.Parse(splitData[5]),float.Parse(splitData[6]));
+        rotation *= Quaternion.Euler(0,0,180);
+        splitData[3] = rotation.x.ToString();
+        splitData[4] = rotation.y.ToString();
+        splitData[5] = rotation.z.ToString();
+        splitData[6] = rotation.w.ToString();
+        data = splitData.Join(delimiter: ",");
+    }
+    // the above modification only works for other players, so this applies the flipped transform to the local player's model
+    [HarmonyPatch(typeof(Animal),nameof(Animal.OnSpawn))]
+    [HarmonyPostfix]
+    public static void PostAnimalSpawn(Animal __instance){
+        if (!__instance.IsLocalPlayer) return;
+        if (!flipped) return;
+        localAnimal = __instance;
+        Transform RigRoot = __instance.Animator.transform.GetChild(0);
+        RigRoot.localRotation = Quaternion.Euler(0, 0, 180);
+        RigRoot.localPosition = new Vector3(0,height,0);
+    }
+
+    void Update(){
+        if (localAnimal == null) return;
+        if (Input.GetKey(KeyCode.LeftBracket)){
+            height -= 0.01f;
+            UpdateLocalAnimalTransform();
+        }
+        if (Input.GetKey(KeyCode.RightBracket)){
+            height += 0.01f;
+            UpdateLocalAnimalTransform();
+        }
+        if (Input.GetKeyDown(KeyCode.Backslash)){
+            flipped = !flipped;
+            UpdateLocalAnimalTransform();
+        }
+    }
+
+    static void UpdateLocalAnimalTransform(){
+        Transform RigRoot = localAnimal.Animator.transform.GetChild(0);
+        if (!flipped){
+            RigRoot.localRotation = Quaternion.Euler(0,0,0);
+            RigRoot.localPosition = Vector3.zero;
+        } else {
+            RigRoot.localRotation = Quaternion.Euler(0,0,180);
+            RigRoot.localPosition = new Vector3(0,height,0);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,15 @@
+# Installation:
+
+- Download [Bepinex](https://github.com/BepInEx/BepInEx/releases/download/v5.4.21/BepInEx_x86_5.4.21.0.zip).
+- Open your game folder (In steam, right click Meadow, Manage > Browse local files).
+- extract Bepinex so that all of it's files are in the game folder.
+- run the game once. This will take some time.
+- close the game.
+- download `Flipped.dll` from [Releases](https://github.com/o7Moon/Meadow.Flipped/releases).
+- move `Flipped.dll` to `(Game Folder from step 2)/BepInEx/plugins/Flipped.dll`.
+
+# Usage
+
+You will be flipped by default. You can toggle this by pressing the `\` key.
+
+The animals have different heights and sizes so it is hard to find a position that works for all of them. As a temporary solution you can use the `[` and `]` keys to adjust the height offset while flipped.
